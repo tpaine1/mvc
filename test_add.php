405de9721018c71855bab4cdfcdef73d0561d880
Thomas Paine <tpaine@kineticsupply.com>
2014-10-15 18:29:17 -0500

    added html to test page

--- test_add.php
+++ test_add.php
@@ -1,5 +1,5 @@
 <?php
-?><h1 THIS IS MY TEST PAGE</h1><?php
+?><h1> THIS IS MY TEST PAGE</h1><?php
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
