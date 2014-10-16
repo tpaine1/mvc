7be398841fa61d6d79b96a5b63252dd7aa6ff848
Thomas Paine <tpaine@kineticsupply.com>
2014-10-15 20:59:59 -0500

    added test2

---
+++ test2.php
@@ -0,0 +1,8 @@
+<?php
+?><h2>test2 page</h2><?php
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
